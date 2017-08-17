3cf83f6ba562ecb62418860439b4463543894a95
Marvella Ortega <Marvella Ortega@192.168.1.70>
2017-08-16 21:38:29 -0500

    Actualizacion 1

---
+++ Nuevo/src/nuevo/Nuevo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo;
+
+/**
+ *
+ * @author Marvella Ortega
+ */
+public class Nuevo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+	calculadora.setVisible(true);
+    }
+    
+}
